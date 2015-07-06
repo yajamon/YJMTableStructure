ebb01b34182d5a5b6db7f257833bc0eab7f43b51
yajamon <yajamon.tatsuki@gmail.com>
2015-07-06 19:32:22 +0900

    Add class "YJMTableSectionList"

---
+++ YJMTableSectionList.h
@@ -0,0 +1,13 @@
+//
+//  YJMTableSectionList.h
+//  Demo
+//
+//  Created by yajamon on 2015/07/06.
+//  Copyright (c) 2015年 yajamon. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface YJMTableSectionList : NSObject
+
+@end
