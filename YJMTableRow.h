318c43cb5d31aec567bf4cdb6e07986b7509ed6b
yajamon <yajamon.tatsuki@gmail.com>
2015-07-06 18:38:46 +0900

    Add class "YJMTableRow"

---
+++ YJMTableRow.h
@@ -0,0 +1,13 @@
+//
+//  YJMTableRow.h
+//  Demo
+//
+//  Created by yajamon on 2015/07/06.
+//  Copyright (c) 2015年 yajamon. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface YJMTableRow : NSObject
+
+@end
---
+++ YJMTableRow.m
@@ -0,0 +1,13 @@
+//
+//  YJMTableRow.m
+//  Demo
+//
+//  Created by yajamon on 2015/07/06.
+//  Copyright (c) 2015年 yajamon. All rights reserved.
+//
+
+#import "YJMTableRow.h"
+
+@implementation YJMTableRow
+
+@end
