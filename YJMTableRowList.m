def187b3f77fe043fffc47f8b8602d1095c50751
yajamon <yajamon.tatsuki@gmail.com>
2015-07-06 18:41:10 +0900

    Add class "YJMTableRowList"

---
+++ YJMTableRowList.m
@@ -0,0 +1,13 @@
+//
+//  YJMTableRowList.m
+//  Demo
+//
+//  Created by yajamon on 2015/07/06.
+//  Copyright (c) 2015年 yajamon. All rights reserved.
+//
+
+#import "YJMTableRowList.h"
+
+@implementation YJMTableRowList
+
+@end
