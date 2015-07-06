90375e17a81ee96d624d97161a7d84debafa8af2
yajamon <yajamon.tatsuki@gmail.com>
2015-07-06 18:42:41 +0900

    Add class "YJMTableSection"

---
+++ YJMTableSection.m
@@ -0,0 +1,13 @@
+//
+//  YJMTableSection.m
+//  Demo
+//
+//  Created by yajamon on 2015/07/06.
+//  Copyright (c) 2015年 yajamon. All rights reserved.
+//
+
+#import "YJMTableSection.h"
+
+@implementation YJMTableSection
+
+@end
